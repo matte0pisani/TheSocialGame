b80b53f86f3665ec5669d171f4fb35af55fa46d9
Matteo Pisani <matteo.pisani.roma@gmail.com>
2021-11-05 22:05:33 +0100

    Soluzione parziale per mail con AWS SES

--- TheSocialGame/TheSocialGame/ForgottenPasswordPage.xaml.cs
+++ TheSocialGame/TheSocialGame/ForgottenPasswordPage.xaml.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net.Mail;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Xamarin.Essentials;
+using Amazon;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using System.Collections.Generic;
 
 namespace TheSocialGame
 {
@@ -18,69 +16,73 @@ namespace TheSocialGame
             InitializeComponent();
         }
 
-        public async Task SendEmail(string subject, string body, List<string> recipients)
-        {
-            try
-            {
-                var message = new EmailMessage
-                {
-                    Subject = subject,
-                    Body = body,
-                    To = recipients,
-                    //Cc = ccRecipients,
-                    //Bcc = bccRecipients
-                };
-                await Email.ComposeAsync(message);
-            }
-            catch (FeatureNotSupportedException fbsEx)
-            {
-                // Email is not supported on this device
-                System.Diagnostics.Debug.Print(fbsEx.ToString());
-            }
-            catch (Exception ex)
-            {
-                // Some other exception occurred
-                System.Diagnostics.Debug.Print(ex.ToString());
-            }
-        }
 
-        void Button_Clicked2(object sender, System.EventArgs e)
-        {
-            try
-            {
-
-                MailMessage mail = new MailMessage();
-                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-
-                mail.From = new MailAddress("matteo.pisani.roma@gmail.com");
-                mail.To.Add(EmailEntry.Text);
-                mail.Subject = "prova email TSG";
-                mail.Body = "messaggio di prova";
-
-                SmtpServer.Port = 587;
-                SmtpServer.Host = "smtp.gmail.com";
-                SmtpServer.EnableSsl = true;
-                SmtpServer.UseDefaultCredentials = false;
-                var psswrd = "bender.1";
-                SmtpServer.Credentials = new System.Net.NetworkCredential("matteo.pisani.roma@gmail.com", psswrd);
-
-                SmtpServer.Send(mail);
-            }
-            catch (Exception ex)
-            {
-                DisplayAlert("Failed", ex.Message, "OK");
-            }
-        }
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
             if (EmailEntry.Text != null && EmailEntry.Text.Length > 0)
             {
-                List<string> recipients = new List<string>();
-                recipients.Add(EmailEntry.Text);
-                await SendEmail("prova email TSG", "Lorem ipsum", recipients);
+                var client = new AmazonSimpleEmailServiceClient("AKIASJHAN5BQHT333L5H", "gd511to6cJ8bX9K0GDcGWCNnphDXjSGaBn+swALo", RegionEndpoint.EUSouth1);
+                SendEmailRequest sendRequest = GenerateDefaultEmailRequest("matteo.pisani.roma@gmail.com", EmailEntry.Text);
+                try
+                {
+                    System.Diagnostics.Debug.Print("Sending email using Amazon SES...");
+                    await client.SendEmailAsync(sendRequest);
+                    System.Diagnostics.Debug.Print("The email was sent successfully.");
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.Print("The email was not sent.");
+                    System.Diagnostics.Debug.Print("Error message: " + ex.Message);
+
+                }
             }
         }
-    }
 
+        private SendEmailRequest GenerateDefaultEmailRequest(string senderAddress, string receiverAddress)
+        {
+            string subject = "Recupero password account TheSocialGame";
+            string textBody = "Amazon SES Test (.NET)\r\n"
+                                        + "This email was sent through Amazon SES "
+                                        + "using the AWS SDK for .NET.";
+            string htmlBody = @"<html>
+                    <head></head>
+                        <body>
+                            <h1>Amazon SES Test (AWS SDK for .NET)</h1>
+                            <p>This email was sent with
+                                <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+                                <a href='https://aws.amazon.com/sdk-for-net/'>
+                                AWS SDK for .NET</a>.</p>
+                        </body>
+             </html>";
+            var sendRequest = new SendEmailRequest
+            {
+                Source = senderAddress,
+                Destination = new Destination
+                {
+                    ToAddresses =
+                        new List<string> { receiverAddress }
+                },
+                Message = new Message
+                {
+                    Subject = new Content(subject),
+                    Body = new Body
+                    {
+                        Html = new Content
+                        {
+                            Charset = "UTF-8",
+                            Data = htmlBody
+                        },
+                        Text = new Content
+                        {
+                            Charset = "UTF-8",
+                            Data = textBody
+                        }
+                    }
+                }
+            };
+
+            return sendRequest;
+        }
+    }
 }
